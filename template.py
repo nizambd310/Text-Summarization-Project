dc8b01a813dcc98af35e80a17543b6b650584a3e
nizambd310 <uddinahmednizam@gmail.com>
2023-12-01 00:34:19 +0600

    last updated november 2023

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'textSumarizer'
+project_name = 'textSummarizer'
 list_of_files = [
     '.github/workflows/.gitkeep',
     f"src/{project_name}/__init__.py",
